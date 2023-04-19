ded1b0c348c7c695e80ff9b15593d5ce76a14713
Faz Kay <fkydln@gmail.com>
2023-04-19 02:56:47 -0700

    slice-and-splice is initiated

---
+++ slice-and-splice/script.js
@@ -0,0 +1,8 @@
+// Slice and Splice You are given two arrays and an index. Copy each element of the first array into the second array, in order. Begin inserting elements at index n of the second array. Return the resulting array. The input arrays should remain the same after the function runs.
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/slice-and-splice
+
+function frankenSplice(arr1, arr2, n) {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
