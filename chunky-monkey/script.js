f864a0176452a85be74eccebc4132fb1bb2a931c
Faz Kay <fkydln@gmail.com>
2023-05-24 19:35:21 -0700

    done

--- chunky-monkey/script.js
+++ chunky-monkey/script.js
@@ -4,21 +4,37 @@
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/chunky-monkey
 
 function chunkArrayInGroups(arr, size) {
-  console.log(Math.floor(arr.length / size));
-  let resultArr = [];
   let transArr = [];
-  for (let index = 0; index < Math.floor(arr.length / size); index++) {
-    const element = arr[index];
-    transArr = arr.slice(0, size);
-    resultArr.push(transArr);
+  while (arr.length > 0) {
+    transArr.push(arr.splice(0, size));
   }
 
-  //   console.log(transArr);
-  console.log(resultArr);
-
-  return arr;
+  return transArr;
 }
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+
+// function chunkArrayInGroups(arr, size) {
+//   //   console.log(Math.floor(arr.length / size));
+//   let resultArr = [];
+//   let transArr = [];
+//   for (let index = 0; index < Math.floor(arr.length / size); index++) {
+//     const element = arr[index];
+//     transArr = arr.slice(0, size);
+//     arr.splice(0, transArr.length);
+//     console.log(transArr);
+//     resultArr.push(transArr);
+//     console.log(arr);
+//   }
+
+//   console.log(resultArr);
+//   console.log("left " + arr);
+//   console.log(transArr);
+//   console.log(resultArr);
+
+//   return arr;
+// }
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
