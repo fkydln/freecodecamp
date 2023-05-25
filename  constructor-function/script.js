381fc7e9b5bf2511011303d9723c7fc03a033970
Faz Kay <fkydln@gmail.com>
2023-05-24 20:12:26 -0700

    done

---
+++  constructor-function/script.js
@@ -0,0 +1,5 @@
+// URL below:https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/object-oriented-programming/define-a-constructor-function
+
+function Dog() {
+  (this.name = "Faz"), (this.color = "yellow"), (this.numLegs = 4);
+}
